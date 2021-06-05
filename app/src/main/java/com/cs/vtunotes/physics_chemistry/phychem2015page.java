a2979932e0481547513c8629ecb5e0180024aa0e
Chethan S <cchethans14@gmail.com>
2021-06-05 22:03:04 +0530

    first commit

--- app/src/main/java/com/cs/vtunotes/physics_chemistry/phychem2015page.java
+++ app/src/main/java/com/cs/vtunotes/physics_chemistry/phychem2015page.java
@@ -3,8 +3,11 @@ package com.cs.vtunotes.physics_chemistry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.cs.vtunotes.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class phychem2015page extends AppCompatActivity {
 
@@ -12,5 +15,26 @@ public class phychem2015page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phychem2015page);
+    }
+
+    public void updateurl(View view) {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.phychemupdateurl);
+
+//                LinearLayout copy = bottomSheetDialog.findViewById(R.id.copyLinearLayout);
+
+
+        bottomSheetDialog.show();
+    }
+
+    public void addsubject(View view) {
+
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.add_subject);
+
+//                LinearLayout copy = bottomSheetDialog.findViewById(R.id.copyLinearLayout);
+
+
+        bottomSheetDialog.show();
     }
 }
