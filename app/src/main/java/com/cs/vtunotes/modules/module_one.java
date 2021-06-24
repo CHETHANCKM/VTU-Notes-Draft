1c80b8a76d785ee197c870b67b0e5e35677c5680
Chethan S <cchethans14@gmail.com>
2021-06-24 11:10:16 +0530

    Version 1

--- app/src/main/java/com/cs/vtunotes/modules/module_one.java
+++ app/src/main/java/com/cs/vtunotes/modules/module_one.java
@@ -1,37 +1,51 @@
 package com.cs.vtunotes.modules;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuCompat;
+import androidx.core.view.MenuItemCompat;
 
+import android.app.SearchManager;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.cs.vtunotes.R;
-import com.cs.vtunotes.editprofileactivity;
 import com.cs.vtunotes.modules.post_notes.post_new_title;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class module_one extends AppCompatActivity {
 
-    TextView head_text;
+//    TextView head_text;
     FloatingActionButton floatingActionButton;
+    MaterialToolbar toolbar;
+
+
     
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_one);
-        head_text = findViewById(R.id.head_text);
+//        head_text = findViewById(R.id.head_text);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         floatingActionButton = findViewById(R.id.floating_addquestion);
+
+
         Intent intent = getIntent();
         String m_subject_code = intent.getStringExtra("subject_code");
         String m_subject_name = intent.getStringExtra("subject_name");
         String m_scheme = intent.getStringExtra("scheme_code");
         String branch = intent.getStringExtra("branch");
         String semester_name = intent.getStringExtra("semester_name");
-        head_text.setText(m_subject_name+" - Module 1");
+        toolbar.setTitle(m_subject_name+" - Module 1");
+//        head_text.setText();
 
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +61,13 @@ public class module_one extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       super.onCreateOptionsMenu(menu);
+       getMenuInflater().inflate(R.menu.top_app_bar, menu);
+       return true;
 
     }
 }
